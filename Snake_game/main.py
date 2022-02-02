590980fb653909eff6f1deb0045ed362bcff3e82
Mitya <you@example.com>
2022-02-02 18:56:10 +0300

    done.

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen, Turtle
 from snake import Snake
+import random
+from food import Food
 
 
 screen = Screen()
@@ -9,22 +11,38 @@ screen.bgcolor("black")
 screen.title("My little Galgamesh. World' eater")
 screen.tracer(0)
 
-game_state_is_on = True
-
 snake = Snake()
 
 screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
-screen.onkey(snake.up(), 'w')
-screen.onkey(snake.down(), 's')
-screen.onkey(snake.left(), 'a')
-screen.onkey(snake.right(), 'd')
+game_state_is_on = True
+food = Turtle("circle")
+food.penup()
+food.goto(random.randint(-300, 300), random.randint(-300, 300))
+food.color("red")
+
 
 while game_state_is_on:
     screen.update()
     time.sleep(0.2)
     snake.move()
 
+    if snake.head.xcor() >= 298 or snake.head.xcor() <= -298 or snake.head.ycor() >= 298 or snake.head.ycor() <= -298:
+        # check on going out of bounds
+        game_state_is_on = False
+
+    if snake.head.distance(food.xcor(), food.ycor()) <= 20:  # condition for eating food
+        snake.add_segment()
+        food.goto(random.randint(-250, 250), random.randint(-250, 250))
+    for i in snake.segments[1:]:
+        if snake.head.pos() == i.pos():
+            game_state_is_on = False
 
 
 screen.exitonclick()
+
+# TODO: scoreboard
--- Snake_game/snake.py
+++ Snake_game/snake.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -11,15 +15,16 @@ class Snake:
         self.segments = []  # all positions of segments
         self.create_snake()
         self.head = self.segments[0]
+        self.last_segment_position = self.segments[-1]
 
     def create_snake(self):
         for i in STARTING_POSITIONS:
-            block = Turtle("square")
-            block.penup()
-            block.goto(i)
-            block.color("white")
+            segment = Turtle("square")
+            segment.penup()
+            segment.goto(i)
+            segment.color("white")
 
-            self.segments.append(block)
+            self.segments.append(segment)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):  # this block make each previous segment take position of a
@@ -28,17 +33,40 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+    def add_segment(self):
+        segment = Turtle("square")
+        segment.penup()
+        segment.color("white")
+        last_x = self.last_segment_position.xcor()
+        last_y = self.last_segment_position.ycor()
+
+        if self.head.heading() == UP:
+            segment.goto(last_x, last_y - 20)
+        if self.head.heading() == DOWN:
+            segment.goto(last_x, last_y + 20)
+        if self.head.heading() == LEFT:
+            segment.goto(last_x + 20, last_y)
+        if self.head.heading() == RIGHT:
+            segment.goto(last_x - 20, last_y)
+
+        self.segments.append(segment)
+
+
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
 
